0d60c8fdc513560828ad60f98c5acb8c712ac386
Louis Dutton <louis@dutton.digital>
2025-01-13 18:17:01 +0000

    setup helix ols

--- flake.nix
+++ flake.nix
@@ -3,50 +3,27 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs =
-    { self, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
       inputs = pkgs: with pkgs; [ odin ];
-    in
-    {
-      defaultPackage = forEachSupportedSystem (
-        { pkgs }:
+    in {
+      defaultPackage = forEachSupportedSystem ({ pkgs }:
         pkgs.stdenv.mkDerivation (finalAttrs: rec {
           name = "game";
           src = self;
           buildInputs = inputs pkgs;
           buildPhase = "odin build src -out=${name}";
           installPhase = "cp ${name} $out";
-        })
-      );
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages =
-              inputs pkgs
-              ++ (with pkgs; [
-                lldb
-                resvg
-                go-task
-                ffmpeg
-              ]);
-          };
-        }
-      );
+        }));
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = inputs pkgs ++ (with pkgs; [ resvg go-task ffmpeg ols ]);
+        };
+      });
     };
 }
--- game/main.odin
+++ game/main.odin
@@ -31,3 +31,4 @@ main :: proc() {
 should_exit :: proc() -> bool {
 	return raylib.WindowShouldClose()
 }
+
